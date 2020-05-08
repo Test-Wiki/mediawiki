40d0706a965cc5538985bacb133f4334e8bc4ceb
MacFan4000 <paulfkeffer@gmail.com>
2020-05-08 14:41:30 -0400

    Update extensions/remove wgGlobalID
    Signed-off-by: MacFan4000 <paulfkeffer@gmail.com>

--- LocalSettings.php
+++ LocalSettings.php
@@ -216,7 +216,6 @@ $wgTitleBlacklistSources = array(
 //MatomoAnalytics
 $wgMatomoAnalyticsServerURL = 'https://piwik.testwiki.wiki/';
 $wgMatomoAnalyticsSiteID = 3;
-$wgMatomoAnalyticsGlobalID = 'false';
 // WikiEditor
 $wgDefaultUserOptions['usebetatoolbar'] = 1;
 $wgDefaultUserOptions['usebetatoolbar-cgd'] = 1;
