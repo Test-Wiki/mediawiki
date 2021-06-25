11d5b8e795c60297bf597995b379fe418468cd46
MacFan4000 <paulfkeffer@gmail.com>
2021-06-24 20:29:30 -0400

    Update LocalSettings.php

--- LocalSettings.php
+++ LocalSettings.php
@@ -357,12 +357,12 @@ $wgFooterIcons = [
 			"src" => $wgResourceBasePath/mages/a/a3/Fosshost.png,
 			"url" => "https://fosshost.org/",
 			"alt" => "Powered by Fosshost",
-		]
+		],
 		"mediawiki" => [
 			// Defaults to point at
 			// "$wgResourceBasePath/resources/assets/poweredby_mediawiki_88x31.png"
 			// plus srcset for 1.5x, 2x resolution variants.
-			"src" => null,
+			"src" => $wgResourceBasePath/resources/assets/poweredby_mediawiki_88x31.png,
 			"url" => "//www.mediawiki.org/",
 			"alt" => "Powered by MediaWiki",
 		]
