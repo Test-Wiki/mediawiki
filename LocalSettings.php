cd02a03b8a53e5d71737d9086daca2d25501f5cf
MacFan4000 <paulfkeffer@gmail.com>
2021-06-24 21:03:18 -0400

    Update LocalSettings.php

--- LocalSettings.php
+++ LocalSettings.php
@@ -348,7 +348,7 @@ $wgCookieWarningGeoIp2Path = '/var/www/html/piwik/misc/GeoLite2-City.mmdb';
 $wgEnablePartialBlocks = true;
 
 //fosshost button
-$wgFooterIcons['poweredby']['fosshost'] = [
+$wgFooterIcons['fosshost']['fosshost'] = [
 	// Defaults to point at
 	// "$wgResourceBasePath/resources/assets/poweredby_mediawiki_88x31.png"
 	// plus srcset for 1.5x, 2x resolution variants.
