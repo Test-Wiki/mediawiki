f7e8fdaa99cb65dedf9c03f9ea19b01195fc418f
MacFan4000 <paulfkeffer@gmail.com>
2023-05-21 19:08:15 -0400

    tweak geoip path

--- LocalSettings.php
+++ LocalSettings.php
@@ -361,5 +361,5 @@ $wgShowDBErrorBacktrace = true;
 $wgCookieWarningEnabled = true;
 $wgCookieWarningGeoIPLookup = 'php';
 $wgCookieWarningGeoIp2 = true;
-$wgCookieWarningGeoIp2Path = '/var/www/html/piwik/misc/GeoLite2-City.mmdb';
+$wgCookieWarningGeoIp2Path = '/var/www/html/piwik/misc/DBIP-City.mmdb';
 $wgEnablePartialBlocks = true;
