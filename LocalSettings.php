e20b1cf16ce67f998318639c40d2fa02f1bc7003
MacFan4000 <paulfkeffer@gmail.com>
2023-06-10 15:00:28 -0400

    no more RO

--- LocalSettings.php
+++ LocalSettings.php
@@ -365,7 +365,6 @@ $wgCookieWarningGeoIPLookup = 'php';
 $wgCookieWarningGeoIp2 = true;
 $wgCookieWarningGeoIp2Path = '/var/www/html/piwik/misc/DBIP-City.mmdb';
 $wgEnablePartialBlocks = true;
-$wgReadOnly = "Upgrading to Debian Bookworm";
 
 $wgParsoidSettings = [
     'linting' => true
