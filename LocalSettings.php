20457a0226e3bb5a3973888db8c29ab103483777
Zippybonzo <90575488+Zippybonzo@users.noreply.github.com>
2023-07-18 18:31:43 +0500

    Update LocalSettings.php - add Checkuser log per community portal

--- LocalSettings.php
+++ LocalSettings.php
@@ -316,6 +316,7 @@ $wgGroupPermissions['steward']['steward'] = true;
 $wgGroupPermissions['steward']['editsitejs'] = true;
 $wgGroupPermissions['steward']['editsitecss'] = true;
 $wgGroupPermissions['steward']['viewpmlog'] = true;
+$wgGroupPermissions['steward']['checkuser-log'] = true;
 $wgGroupPermissions['sysadmin']['editsitecss'] = true;
 $wgGroupPermissions['sysadmin']['editsitejs'] = true;
 $wgGroupPermissions['sysadmin']['siteadmin'] = true;
