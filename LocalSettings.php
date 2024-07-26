340ffaa6b0f4156e445c389464300e0fb71abf75
Justarandomamerican <76439161+Justarandomamerican@users.noreply.github.com>
2024-07-25 18:53:09 -0600

    Update LocalSettings.php

--- LocalSettings.php
+++ LocalSettings.php
@@ -568,3 +568,4 @@ $wgNamespaceAliases['TW'] = NS_PROJECT;
 $wgNamespaceAliases['TWT'] = NS_PROJECT_TALK;
 $wgPygmentizePath = "/usr/local/bin/pygmentize";
 $wgHCaptchaSiteKey = '6902a03c-cb28-4e42-9a5e-f7aae9a8175c';
+$wgSFSDenyListCacheDuration = '86400';
