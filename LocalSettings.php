a25cc4a4b120aed11c79f5531185d8bd9881da27
Justarandomamerican <76439161+Justarandomamerican@users.noreply.github.com>
2024-07-27 16:21:34 -0600

    Update LocalSettings.php

--- LocalSettings.php
+++ LocalSettings.php
@@ -657,4 +657,5 @@ $wgSFSDenyListCacheDuration = '86400';
 $wgCaptchaWhitelist =
 		'#^(https?:)?//([.a-z0-9-]+\\.)?((wikimedia|wikipedia|wiktionary|wikiquote|wikibooks|wikisource|wikispecies|mediawiki|wikinews|wikiversity|wikivoyage|wikidata|wikifunctions|wmflabs)\.org'
 		. '|dnsstuff\.com|completewhois\.com|wikimedia\.de)([?/\#]|$)#i';
+$wgTranslateDocumentationLanguageCode = 'qqq';
 $wgPageLanguageUseDB = true;
